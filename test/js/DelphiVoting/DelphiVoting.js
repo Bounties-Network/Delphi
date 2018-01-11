ec5fb0c0c2f2d63ac88e79ae645f134d284bee24
Mike Goldin <mike.goldin@consensys.net>
2018-01-10 19:40:16 -0500

    Add DelphiVoting constructor test

--- test/js/DelphiVoting/DelphiVoting.js
+++ test/js/DelphiVoting/DelphiVoting.js
@@ -1,9 +1,23 @@
 /* eslint-env mocha */
 /* global contract artifacts assert */
 
+const DelphiVoting = artifacts.require('DelphiVoting');
+const Registry = artifacts.require('Registry');
+const Parameterizer = artifacts.require('Parameterizer');
+
 contract('DelphiVoting', () => {
   describe('Function: DelphiVoting', () => {
-    it('should instantiate the contract with the expected values');
+    it('should instantiate the contract with the expected values', async () => {
+      const dv = await DelphiVoting.deployed();
+
+      const storedArbiterSet = await dv.arbiterSet.call();
+      assert.strictEqual(storedArbiterSet, Registry.address,
+        'the arbiter set was not initialized properly');
+
+      const storedParameterizer = await dv.parameterizer.call();
+      assert.strictEqual(storedParameterizer, Parameterizer.address,
+        'the parameterizer was not initialized properly');
+    });
   });
 });
 
