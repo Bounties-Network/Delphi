bea3be5b5953a41f01b22002fc816d76039f86f4
mbeylin <mark_beylin@hotmail.com>
2018-01-12 09:48:54 -0500

    added the settlements user flow

--- contracts/DelphiStake.sol
+++ contracts/DelphiStake.sol
@@ -18,6 +18,14 @@ contract DelphiStake {
       uint ruling;
       bool ruled;
       bool paid;
+      bool settlementFailed;
+      Settlement[] settlements;
+    }
+
+    struct Settlement {
+      uint amount;
+      bool stakerAgrees;
+      bool claimantAgrees;
     }
 
     uint public stake;
@@ -77,6 +85,17 @@ contract DelphiStake {
         _;
     }
 
+    modifier settlementFailed(uint _claimId){
+        require(claims[_claimId].settlementFailed);
+        _;
+    }
+
+    modifier onlyStakerOrClaimant(uint _claimId){
+        require(msg.sender == staker || msg.sender == claims[_claimId].claimant);
+        _;
+    }
+
+
 
     function DelphiStake(uint _value, address _tokenAddress, string _data, uint _lockupPeriod, address _arbiter)
     public
@@ -100,7 +119,7 @@ contract DelphiStake {
     transferredAmountEqualsValue(_fee)
     stakerCanPay(_amount, _fee)
     {
-        claims.push(Claim(msg.sender, _amount, _fee, 0, _data, 0, false, false));
+        claims.push(Claim(msg.sender, _amount, _fee, 0, _data, 0, false, false, false));
         openClaims ++;
         stake -= (_amount + _fee);
         // the claim amount and claim fee are locked up in this contract until the arbiter rules
@@ -116,10 +135,56 @@ contract DelphiStake {
       claims[_claimId].surplusFee += _amount;
     }
 
+    function proposeSettlement(uint _claimId, uint _amount)
+    public
+    onlyStakerOrClaimant(_claimId)
+    {
+      require(claims[_claimId].amount + claims[_claimId].fee >= _amount);
+      // only allows settlements for up to the amount that's already been staked by the staker as pertaining to this case
+
+      if (msg.sender == staker){
+        claims[_claimId].settlements.push(Settlement(_amount, true, false));
+      } else {
+        claims[_claimId].settlements.push(Settlement(_amount, false, true));
+      }
+    }
+
+    function acceptSettlement(uint _claimId, uint _settlementId)
+    onlyStakerOrClaimant(_claimId)
+    {
+      if (msg.sender == staker){
+        claims[_claimId].settlements[_settlementId].stakerAgrees = true;
+      } else {
+        claims[_claimId].settlements[_settlementId].claimantAgrees = true;
+      }
+
+      if (claims[_claimId].settlements[_settlementId].claimantAgrees &&
+          claims[_claimId].settlements[_settlementId].stakerAgrees &&
+          !claims[_claimId].settlementFailed
+          !claims[_claimId].ruled){
+        claims[_claimId].ruled = true;
+        claims[_claimId].claimant.transfer(claims[_claimId].settlements[_settlementId].amount + claims[_claimId].fee);
+        stake += (claims[_claimId].amount + claims[_claimId].fee - claims[_claimId].settlements[_settlementId].amount);
+
+      }
+
+      // TODO: doesn't cover the case where the settlement amount is greater than the sum of the fees
+    }
+
+    function settlementFailed(uint _claimId)
+    public
+    payable
+    transferredAmountEqualsValue(_amount)
+    onlyStakerOrClaimant(_claimId)
+    {
+      claims[_claimId].settlementFailed = true;
+    }
+
     function ruleOnClaim(uint _claimId, uint _ruling)
     public
     onlyArbiter
     claimNotRuled(_claimId)
+    settlementFailed(_claimId)
     {
         claims[_claimId].ruled = true;
         claims[_claimId].ruling = _ruling;
