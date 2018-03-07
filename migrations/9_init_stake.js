69ba40ed8bb5e08355bfecaf304532ef4c9b2d67
Mike Goldin <mike.goldin@consensys.net>
2018-03-07 15:58:10 -0500

    Initialize the DelphiStake in deployment

---
+++ migrations/9_init_stake.js
@@ -0,0 +1,30 @@
+/* global artifacts */
+
+const DelphiVoting = artifacts.require('DelphiVoting.sol');
+const DelphiStake = artifacts.require('DelphiStake.sol');
+const Token = artifacts.require('tokens/eip20/EIP20.sol');
+
+const fs = require('fs');
+
+module.exports = (deployer, network) => {
+  deployer.then(async () => {
+    const ds = await DelphiStake.deployed();
+    const conf = JSON.parse(fs.readFileSync('./conf/config.json'));
+
+    let arbiter = conf.arbiter;
+    let token = conf.token;
+
+    // If we are testing, using whatever token and DV instances were deployed by this migrator,
+    // and approve the DS instance to transfer from the token.
+    if (network === 'test') {
+      arbiter = (await DelphiVoting.deployed()).address;
+      token = (await Token.deployed()).address;
+
+      await (await Token.at(token))
+        .approve((await DelphiStake.deployed()).address, conf.initialStake);
+    }
+
+    return ds.initDelphiStake(conf.initialStake, token, conf.data, conf.lockupPeriod, arbiter);
+  });
+};
+
