344f227975cc295d087efe3e0d9862ed6e707a8d
mbeylin <mark_beylin@hotmail.com>
2018-04-24 20:30:31 -0400

    tests all working

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
-    testCommand: '../node_modules/.bin/truffle test --network coverage -p 7545',
-    skipFiles: ['deps/']
+    testCommand: '../node_modules/.bin/truffle test --network coverage -p 7545 -m candy maple cake sugar pudding cream honey rich smooth crumble sweet treat',
+    skipFiles: ['/deps/']
     port: 8555,
-    testrpcOptions: "--mnemonic candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
+    testrpcOptions: "-m candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
 };
--- contracts/DelphiVoting.sol
+++ contracts/DelphiVoting.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.23;
+pragma solidity ^0.4.18;
 
 import "./deps/Registry.sol";
 import "./deps/Parameterizer.sol";
