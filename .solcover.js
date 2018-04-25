0ca1dfb83542ce8c347c1c6d2ea30d67b410b10c
mbeylin <mark_beylin@hotmail.com>
2018-04-24 20:04:20 -0400

    removed fixes, added mnemonic

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
     testCommand: '../node_modules/.bin/truffle test --network coverage -p 7545',
     skipFiles: ['deps/']
-    port: 8555
-
+    port: 8555,
+    testrpcOptions: "--mnemonic candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
 };
