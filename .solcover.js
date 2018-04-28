2db9711f06995cea17fc455bb070f38817fc2c74
Mike Goldin <mike.goldin@consensys.net>
2018-04-28 14:48:20 -0400

    Remove unnecessary specifications in coverage.js
    Coverage.js is overspecified.
    
    * Remove skipFiles, since we no longer have a deps directory
    * Remove port 8555 specification, since the coverage network already
      specifies port 7545.
    * In test command, remove the port and mnemonic specifications. The port
      spec is unnecessary since it's specified for the coverage network in
      the truffle.js The mnemonic spec can be moved to testrpcOptions.
    * Move the port and mnemonic specifications to testrpcOptions

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,8 @@
 module.exports = {
-    testCommand: '../node_modules/.bin/truffle test --network coverage -p 7545 -m candy maple cake sugar pudding cream honey rich smooth crumble sweet treat',
-    skipFiles: ['/deps/']
-    port: 8555,
-    testrpcOptions: "-m candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
+  // use the local version of truffle
+  testCommand: '../node_modules/.bin/truffle test --network coverage',
+  // start blockchain on the same port specified in truffle.js
+  // use the default delicious Ganache mnemonic
+  testrpcOptions: '-p 7545 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"'
 };
+
