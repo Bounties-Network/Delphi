07e370687bb58c60a4cbbf9b7b65042120a77a63
grana69 <vgrana@uabc.edu.mx>
2018-05-08 12:23:56 -0600

    claimFee fixed

--- test/js/DelphiVoting/claimFee.js
+++ test/js/DelphiVoting/claimFee.js
@@ -13,7 +13,7 @@ const config = JSON.parse(fs.readFileSync('./conf/tcrConfig.json'));
 
 contract('DelphiVoting', (accounts) => {
   describe('Function: claimFee', () => {
-    const [staker, claimant, arbiterAlice, arbiterBob, arbiterCharlie] = accounts;
+    const [staker, claimant, arbiterAlice, arbiterBob, arbiterCharlie, thirdPary] = accounts;
 
     before(async () => {
       const ds = await DelphiStake.deployed();
@@ -128,7 +128,7 @@ contract('DelphiVoting', (accounts) => {
 
         // Make a new claim and compute its claim ID.
         const claimNumber = // should be one, since we have already made one claim (0)
-        await utils.makeNewClaim(staker, claimant, CLAIM_AMOUNT, FEE_AMOUNT, 'i love cats');
+          await utils.makeNewClaim(staker, claimant, CLAIM_AMOUNT, FEE_AMOUNT, 'i love cats');
         const claimId = utils.getClaimId(ds.address, claimNumber.toString(10));
 
         // Arbiters commit votes. Charlie commits the non-plurality vote.
@@ -213,70 +213,77 @@ contract('DelphiVoting', (accounts) => {
       // Revisit this test after implementing safemath.
     });
 
-    it('should revert if called by anyone but one of the arbiters');
-    it('should not allow an arbiter to claim a fee when they did not commit');
-    it('should not allow an arbiter to claim a fee when they committed but did not reveal');
-    it('should not allow an arbiter to claim a fee for a claim where they committed and ' +
-      'revealed, but for which a ruling has not been submitted', async () => {
+    it('should revert if called by anyone but one of the arbiters', async () => {
       const dv = await DelphiVoting.deployed();
-      const ds = await DelphiStake.deployed();
-      const token = EIP20.at(await ds.token.call());
+
+      // Use previous claim, since we have two arbiters who still have not claimed for it
+      const CLAIM_NUMBER = '1';
+      const PLURALITY_VOTE = '1';
+      const SALT = '420';
+
+      try {
+        // Check the requiere onlyArbiters
+        await utils.as(thirdPary, dv.claimFee, DelphiStake.address, CLAIM_NUMBER,
+          PLURALITY_VOTE, SALT);
+      } catch (err) {
+        assert(utils.isEVMRevert(err), err.toString());
+        return;
+      }
+      assert(false, 'Expetected to revert if the reveal period is active');
+    });
+    it('should not allow an arbiter to claim a fee when they did not commit', async () => {
+      const dv = await DelphiVoting.deployed();
 
       // Set constants
       const CLAIM_AMOUNT = '10';
       const FEE_AMOUNT = '10';
-      const PLURALITY_VOTE = '0';
+      const VOTE = '1';
       const SALT = '420';
 
-      // Compute a secret hash to submit
-      const secretHash = utils.getSecretHash(PLURALITY_VOTE, SALT);
-
-      // Make a new claim and compute its claim ID.
-      const claimNumber = // should be two, since we have already made two claims
+      // Make a new claim and get its claimId
+      const claimNumber = // should be zero, since this is the first test
         await utils.makeNewClaim(staker, claimant, CLAIM_AMOUNT, FEE_AMOUNT, 'i love cats');
-      const claimId = utils.getClaimId(ds.address, claimNumber.toString(10));
 
-      // Arbiter commits vote.
+      try {
+        // Check the requiere onlyArbiters
+        await utils.as(arbiterAlice, dv.claimFee, DelphiStake.address, claimNumber,
+          VOTE, SALT);
+      } catch (err) {
+        assert(utils.isEVMRevert(err), err.toString());
+        return;
+      }
+      assert(false, 'Expetected to not allow an arbiter to claim a fee when they did not commit');
+    });
+    it('should not allow an arbiter to claim a fee when they committed but did not reveal', async () => {
+      const dv = await DelphiVoting.deployed();
+      const ds = await DelphiStake.deployed();
+
+      // Set constants
+      const CLAIM_AMOUNT = '10';
+      const FEE_AMOUNT = '10';
+      const VOTE = '1';
+      const SALT = '420';
+
+      // Make a new claim and get its claimId
+      const claimNumber = // should be zero, since this is the first test
+        await utils.makeNewClaim(staker, claimant, CLAIM_AMOUNT, FEE_AMOUNT, 'i love cats');
+
+      // Get the secret hash for the salted vote
+      const secretHash = utils.getSecretHash(VOTE, SALT);
+
+      // Commit vote
       await utils.as(arbiterAlice, dv.commitVote, ds.address, claimNumber, secretHash);
 
       // Increase time to get to the reveal phase
       await utils.increaseTime(config.paramDefaults.commitStageLength + 1);
 
-      // Arbiter reveals votes
-      await utils.as(arbiterAlice, dv.revealVote, claimId, PLURALITY_VOTE, SALT);
-
-      // Increase time to finish the reveal phase so we can submit the ruling
-      await utils.increaseTime(config.paramDefaults.revealStageLength);
-
-      // Capture Alice's starting token balance, claim the fee and get her final balance
-      const startingBalanceAlice = await token.balanceOf(arbiterAlice);
-
-      // Give the DelphiVoting contract a balance prematurely so that it can trasfer tokens
-      // to the early-claimer
-      await utils.as(staker, token.transfer, dv.address, FEE_AMOUNT);
-
-      // Alice attempts to claim a fee, though the ruling has not been submitted yet. This
-      // should fail!
       try {
-        await utils.as(arbiterAlice, dv.claimFee, DelphiStake.address, claimNumber,
-          PLURALITY_VOTE, SALT);
+        await utils.as(arbiterAlice, dv.claimFee, DelphiStake.address, claimNumber, VOTE, SALT);
       } catch (err) {
         assert(utils.isEVMRevert(err), err.toString());
-
-        const finalBalanceAlice = await token.balanceOf(arbiterAlice);
-        assert.strictEqual(finalBalanceAlice.toString(10), startingBalanceAlice.toString(10),
-          'Alice was able to claim a reward before submitting a ruling');
+        return;
       }
-
-      // Submit ruling. Now Alice should be able to claim her due.
-      await utils.as(arbiterAlice, dv.submitRuling, DelphiStake.address, claimNumber);
-      await utils.as(arbiterAlice, dv.claimFee, DelphiStake.address, claimNumber,
-        PLURALITY_VOTE, SALT);
-
-      const finalBalanceAlice = await token.balanceOf(arbiterAlice);
-      const expectedFinalBalanceAlice = startingBalanceAlice.plus(FEE_AMOUNT);
-      assert.strictEqual(finalBalanceAlice.toString(10), expectedFinalBalanceAlice.toString(10),
-        'After submitting the ruling, Alice still was unable to claim her fee');
+      assert(false, 'Expetected to not allow an arbiter to claim a fee when they committed but did not reveal');
     });
   });
 });
