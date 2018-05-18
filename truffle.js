1569c3ee770d07e27a69799c45740034db3656c1
Matt Garnett <matt@mattgarnett.com>
2018-05-18 14:08:18 -0700

    started work on migrations

--- truffle.js
+++ truffle.js
@@ -19,11 +19,11 @@ module.exports = {
     },
     // config for solidity-coverage
     coverage: {
-      host: 'localhost',
+      host: 'ganache',
       network_id: '*',
-      port: 7545, // <-- If you change this, also set the port option in .solcover.js.
-      gas: 0xfffffffffff, // <-- Use this high gas value
-      gasPrice: 0x01, // <-- Use this low gas price
+      port: 8545, // <-- If you change this, also set the port option in .solcover.js.
+      // gas: 0xfffffffffff, // <-- Use this high gas value
+      // gasPrice: 0x01, // <-- Use this low gas price
     },
   },
 };
