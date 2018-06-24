85b2f4f63017092fada8164491e3ae8b97401e3a
Mike Goldin <mike.goldin@consensys.net>
2018-06-24 18:08:50 -0400

    Update claimFee tests to work in factory context

--- test/js/utils.js
+++ test/js/utils.js
@@ -2,7 +2,6 @@
 
 const Registry = artifacts.require('Registry.sol');
 const DelphiStake = artifacts.require('DelphiStake.sol');
-const DelphiStakeFactory = artifacts.require('DelphiStakeFactory');
 const EIP20 = artifacts.require('tokens/eip20/EIP20.sol');
 
 const HttpProvider = require('ethjs-provider-http');
@@ -28,15 +27,12 @@ const utils = {
       arbiter, { from: staker });
   },
 
-  makeNewClaim: async (staker, claimant, amount, fee, data) => {
-    const df = await DelphiStakeFactory.deployed();
-    const ds = await DelphiStake.at(await df.stakes.call('0'));
-
+  makeNewClaim: async (staker, claimant, amount, fee, data, ds) => {
     const token = EIP20.at(await ds.token.call());
 
     await utils.as(staker, ds.whitelistClaimant, claimant, delphiConfig.deadline);
 
-    await utils.as(claimant, token.approve, ds.address, new BN(amount, 10).plus(new BN(fee, 10)));
+    await utils.as(claimant, token.approve, ds.address, new BN(fee, 10));
     await utils.as(claimant, ds.openClaim, amount, fee, data);
 
     const claimId = await ds.getNumClaims();
