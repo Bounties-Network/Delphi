6580d7b2c9d8327473f714c4b56d4352b8a445e1
mbeylin <mark_beylin@hotmail.com>
2018-08-03 22:07:24 -0400

    parameterized the staker

--- contracts/DelphiStakeFactory.sol
+++ contracts/DelphiStakeFactory.sol
@@ -21,15 +21,19 @@ contract DelphiStakeFactory {
   /*
   @dev when creating a new Delphi Stake using a proxy contract architecture, a user must
   initialialize their stake, depositing their tokens
+  @param _staker the address of the individual creating the stake
   @param _value the value of the stake in token units
   @param _token the address of the token being deposited
   @param _data a content hash of the relevant associated data describing the stake
   @param _claimDeadline the deadline for opening new cliams; the earliest moment that
   a stake can be withdrawn by the staker
   */
-  function createDelphiStake(uint _value, EIP20 _token, string _data, uint _stakeReleaseTime)
+  function createDelphiStake(address _issuer, uint _value, EIP20 _token, string _data, uint _stakeReleaseTime)
   public
   {
+    // Revert if the issuer is the 0 _contractAddress
+    require(_issuer != address(0));
+    
     // Revert if the specified value to stake cannot be transferred in
     require(_token.transferFrom(msg.sender, this, _value));
 
@@ -40,7 +44,7 @@ contract DelphiStakeFactory {
     _token.approve(newStake, _value);
 
     // Initialize the stake and set the staker address as the msg.sender
-    stakes[stakes.length - 1].initDelphiStake(msg.sender, _value, _token, _data, _stakeReleaseTime);
+    stakes[stakes.length - 1].initDelphiStake(_issuer, _value, _token, _data, _stakeReleaseTime);
 
     StakeCreated(stakes.length - 1, stakes[stakes.length - 1]);
   }
