ebb237f65cb3d83099d56e7660e116d129ca8a0b
Matt Garnett <matt@mattgarnett.com>
2018-11-04 21:16:02 +0100

    added tests for accept settlement

--- test/utils/config.js
+++ test/utils/config.js
@@ -6,7 +6,8 @@ const stake = {
   "minFee": 10,
   "claimDeadline": 99999999999999999999999,
   "claimAmount": 5,
-  "settlementAmount": 3
+  "settlementAmount": 3,
+  "claimantBalance": 100
 }
 
 module.exports = stake;
